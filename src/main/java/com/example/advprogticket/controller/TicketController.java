548cc46fac56b9ce4b59d55edc382d624f6fe09a
Ruben Boone <r0785519@student.thomasmore.be>
2022-01-04 14:09:38 +0100

    base

--- src/main/java/com/example/advprogticket/controller/TicketController.java
+++ src/main/java/com/example/advprogticket/controller/TicketController.java
@@ -1,4 +1,41 @@
 package com.example.advprogticket.controller;
 
+import com.example.advprogticket.model.Ticket;
+import com.example.advprogticket.repository.TicketRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.PostConstruct;
+import java.util.Date;
+import java.util.List;
+
+@RestController
 public class TicketController {
+
+    @Autowired
+    private TicketRepository ticketRepository;
+
+    @PostConstruct
+    void fillDb(){
+        if (ticketRepository.count() == 0){
+            ticketRepository.save(new Ticket(1, "BET001", "Ruben", "Boone", "r0785519@student.thomasmore.be", new Date()));
+            ticketRepository.save(new Ticket(2, "FRT001", "Ruben", "Boone", "r0785519@student.thomasmore.be", new Date()));
+            ticketRepository.save(new Ticket(3, "FRT001", "Ruben", "Boone", "r0785519@student.thomasmore.be", new Date()));
+            ticketRepository.save(new Ticket(4, "UST001", "Ruben", "Boone", "r0785519@student.thomasmore.be", new Date()));
+            ticketRepository.save(new Ticket(5, "BET001", "Ruben", "Boone", "r0785519@student.thomasmore.be", new Date()));
+            ticketRepository.save(new Ticket(6, "BET001", "Ruben", "Boone", "r0785519@student.thomasmore.be", new Date()));
+            ticketRepository.save(new Ticket(7, "BET002", "Ruben", "Boone", "r0785519@student.thomasmore.be", new Date()));
+            ticketRepository.save(new Ticket(8, "BET002", "Ruben", "Boone", "r0785519@student.thomasmore.be", new Date()));
+        }
+
+        System.out.println("Info test: " + ticketRepository);
+
+    }
+
+    @GetMapping("/tickets")
+    public List<Ticket> getAllTickets(){
+        return ticketRepository.findAll();
+    }
+
 }
